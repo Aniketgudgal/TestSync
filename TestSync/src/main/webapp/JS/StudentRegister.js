4319b9096c3843c70427d64915fc1019751b0f6a
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-05-14 00:49:50 +0530

    Student Registration page select option added

--- TestSync/src/main/webapp/JS/StudentRegister.js
+++ TestSync/src/main/webapp/JS/StudentRegister.js
@@ -1,12 +1,44 @@
- window.onload = function(){
-	loadCourses();
- }
+// window.onload = function(){
+//	loadCourses();
+ //}
  
- function loadCourses(){
-	fetch("GetCourseController").then(response => response.text()).then(data =>{
-		document.getElementById("choosecourse").innerHTML += data;
-	}).catch(error =>{
-		console.log(error);
-	});
-	
+ window.onload = function () {
+     loadCourses();
  }
+
+ function loadCourses(){
+
+     fetch("/TestSync/getCourseController")
+     .then(response => response.text())
+     .then(data => {
+
+         document.getElementById("choosecourse").innerHTML =
+             '<option value="">Select course</option>' + data;
+
+         console.log(data);
+
+     })
+     .catch(error => {
+
+         console.log(error);
+
+     });
+
+ }
+/*
+ function loadCourses()
+ {
+ 	let data = document.getElementById("choosecourse");
+ 	let aj = new XMLHttpRequest()
+	console.log("click");
+ 	aj.open("GET" , "/TestSync/GetCourseController",true);
+ 	aj.onreadystatechange = function(){
+ 		if(this.readyState == 4 && this.status == 200)
+ 			{
+ 				console.log(this.responseText);
+ 				data.innerHTML = this.responseText;
+ 			}
+ 	};
+ 	aj.send();
+ }
+ */
---
+++ TestSync/src/main/webapp/JS/ValidationOnData.js
@@ -0,0 +1,4 @@
+function addSchedule()
+{
+	return confirm('Are You Sure to Schedule Exam?');
+}
